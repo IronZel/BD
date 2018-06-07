1542a6cd897e523d198614b6190123c3b5fc965b
SmartGit <nice.maximenko1@mail.ru>
2018-06-07 17:26:42 +0300

    создание sql-запросов для таблицы document_change

---
+++ DB/queries/documents_change/delete/delete act.sql
@@ -0,0 +1,5 @@
+﻿/* Запрос на удаление данных */
+
+$DELETE 
+FROM `document_change` 
+WHERE `act`=`act`;
---
+++ DB/queries/documents_change/insert_documents.sql
@@ -0,0 +1,4 @@
+﻿/*Запрос на добавление данных*/
+
+$INSERT INTO `document_change`(`doc_changeable`, `doc_changer`, `act`) 
+VALUES ([value-1],[value-2],[value-3]);
