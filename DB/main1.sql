a1ad7db8580e96679a1b53866c88e73549dc2b4d
SmartGit <nice.maximenko1@mail.ru>
2018-06-08 11:33:42 +0300

    финальная бд

--- DB/main1.sql
+++ DB/main1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 08 2018 г., 11:19
+-- Время создания: Июн 08 2018 г., 11:32
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `Administration`
+-- База данных: `main1`
 --
 
 -- --------------------------------------------------------
@@ -29,11 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `documents` (
-  `ID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `date_accept` date NOT NULL,
   `date_entry` date DEFAULT NULL,
-  `head_ID` int(11) NOT NULL
+  `head_id` int(11) NOT NULL,
+  `doc_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -43,9 +44,9 @@ CREATE TABLE `documents` (
 --
 
 CREATE TABLE `documents_group` (
-  `ID` int(11) NOT NULL,
-  `doc_ID` int(11) NOT NULL,
-  `group_ID` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `doc_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -55,10 +56,21 @@ CREATE TABLE `documents_group` (
 --
 
 CREATE TABLE `document_change` (
-  `act_ID` int(11) NOT NULL,
+  `act_id` int(11) NOT NULL,
   `doc_changeable` int(11) NOT NULL,
   `doc_changer` int(11) NOT NULL,
   `act` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `group`
+--
+
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -68,7 +80,7 @@ CREATE TABLE `document_change` (
 --
 
 CREATE TABLE `heads` (
-  `ID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `surname` varchar(20) NOT NULL,
   `patronymic` varchar(30) NOT NULL
@@ -82,20 +94,20 @@ CREATE TABLE `heads` (
 -- Индексы таблицы `documents`
 --
 ALTER TABLE `documents`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `headID` (`head_ID`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `headID` (`head_id`);
 
 --
 -- Индексы таблицы `documents_group`
 --
 ALTER TABLE `documents_group`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `document_change`
 --
 ALTER TABLE `document_change`
-  ADD PRIMARY KEY (`act_ID`),
+  ADD PRIMARY KEY (`act_id`),
   ADD KEY `doc_changeable` (`doc_changeable`),
   ADD KEY `doc_changer` (`doc_changer`);
 
@@ -103,7 +115,7 @@ ALTER TABLE `document_change`
 -- Индексы таблицы `heads`
 --
 ALTER TABLE `heads`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -113,25 +125,25 @@ ALTER TABLE `heads`
 -- AUTO_INCREMENT для таблицы `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `documents_group`
 --
 ALTER TABLE `documents_group`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `document_change`
 --
 ALTER TABLE `document_change`
-  MODIFY `act_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `act_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `heads`
 --
 ALTER TABLE `heads`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
